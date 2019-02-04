e89eca099cf3222e3aac4bb8913a1718fcc500c0
bradleypmartin <brad.martin@ibotta.com>
2019-02-03 18:34:10 -0700

    dice roll complete. on to DRY principles and motion detection.

--- Dicee App/ViewController.swift
+++ Dicee App/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let diceImageArray = ["dice1",
+                          "dice2",
+                          "dice3",
+                          "dice4",
+                          "dice5",
+                          "dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var diceIndex1: Int = 0
+    var diceIndex2: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceIndex1 = Int.random(in: 0...5)
+        diceIndex2 = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named: diceImageArray[diceIndex1])
+        diceImageView2.image = UIImage(named: diceImageArray[diceIndex2])
+        
+    }
+    
 }
 
