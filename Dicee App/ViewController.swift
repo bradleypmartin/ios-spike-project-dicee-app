47ad4ac4ef88d7ed8cebcdfdf543b2675460ba0e
bradleypmartin <brad.martin@ibotta.com>
2019-02-03 18:41:49 -0700

    DRYed up the dice roll code (and included on app launch)

--- Dicee App/ViewController.swift
+++ Dicee App/ViewController.swift
@@ -25,16 +25,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     @IBAction func rollButton(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         diceIndex1 = Int.random(in: 0...5)
         diceIndex2 = Int.random(in: 0...5)
         
         diceImageView1.image = UIImage(named: diceImageArray[diceIndex1])
         diceImageView2.image = UIImage(named: diceImageArray[diceIndex2])
-        
     }
     
 }
